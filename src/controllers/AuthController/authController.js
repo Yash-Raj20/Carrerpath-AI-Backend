f63ce4e43535b020d5ea48ee6757f9544c26ed0e
Yash-Raj20 <ratneshkumarstm987@gmail.com>
2025-07-06 00:21:02 +0530

    cors error solve

--- src/controllers/AuthController/authController.js
+++ src/controllers/AuthController/authController.js
@@ -8,8 +8,8 @@ const sendTokenResponse = (user, res, statusCode = 200) => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'none',
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
